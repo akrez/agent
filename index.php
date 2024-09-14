953a31bc853929e78ae749210744ad2095355901
Akrez <akrez.like@gmail.com>
2024-09-14 22:49:04 +0330

    update+

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Psr7\MultipartStream;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\ServerRequest;
 use GuzzleHttp\Psr7\Uri;
+use Laminas\HttpHandlerRunner\Emitter\SapiStreamEmitter;
+use Psr\Http\Message\ServerRequestInterface;
 
 require_once './vendor/autoload.php';
 
-function extractUri()
+function requestFromGlobals()
 {
-    $basePath = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-    $slashedTargetUrl = substr($_SERVER['REQUEST_URI'], strlen($basePath));
+    return ServerRequest::fromGlobals();
+}
+
+function getNewUri()
+{
+    if (true) {
+        $slashedTargetUrl = $_SERVER['PATH_INFO'];
+    } else {
+        $basePath = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+        $slashedTargetUrl = substr($_SERVER['REQUEST_URI'], strlen($basePath));
+    }
+
+    $slashedTargetUrl = ltrim($slashedTargetUrl, " \n\r\t\v\0/");
     $parts = explode('/', $slashedTargetUrl, 2) + array_fill(0, 2, null);
 
     if (
@@ -22,6 +40,93 @@ function extractUri()
     return null;
 }
 
-$newUri = extractUri();
+function getBoundary(ServerRequestInterface $serverRequest): ?string
+{
+    $contentType = $serverRequest->getHeaderLine('Content-Type');
+
+    if (
+        strpos($contentType, 'multipart/form-data') === 0 and
+        $serverRequest->getMethod() === 'POST' and
+        preg_match('/boundary=(.*)$/', $contentType, $matches)
+    ) {
+        return trim($matches[1], '"');
+    }
+
+    return null;
+}
+
+function setMultipart(string $boundaryName, ServerRequestInterface $request)
+{
+    $elements = [];
+
+    foreach ($request->getParsedBody() as $key => $value) {
+        $elements[] = [
+            'name' => $key,
+            'contents' => $value,
+        ];
+    }
+
+    foreach ($request->getUploadedFiles() as $key => $value) {
+        if (empty($value->getError())) {
+            $elements[] = [
+                'name' => $key,
+                'filename' => $value->getClientFilename(),
+                'contents' => $value->getStream(),
+            ];
+        }
+    }
+
+    $body = new MultipartStream($elements, $boundaryName);
+
+    return $request->withBody($body);
+}
+
+function send($request, $clientConfig = [])
+{
+    $timeout = ini_get('max_execution_time') ?? 60;
+
+    $defaultConfig = [
+        'timeout' => $timeout,
+        'connect_timeout' => $timeout,
+        'read_timeout' => $timeout,
+        'verify' => false,
+        'allow_redirects' => false,
+        'referer' => false,
+    ];
+
+    $client = new Client(array_replace_recursive(
+        $defaultConfig,
+        $clientConfig
+    ));
+
+    try {
+        return $client->send($request);
+    } catch (ClientException $e) {
+        return $e->getResponse();
+    } catch (ServerException $e) {
+        return $e->getResponse();
+    } catch (Throwable $e) {
+        return new Response(500, [], json_encode((array) $e), 1.1, 'Internal Server Throwable Error');
+    } catch (Exception $e) {
+        return new Response(500, [], json_encode((array) $e), 1.1, 'Internal Server Exception Error');
+    }
+}
+
+$newUri = getNewUri();
 
 $request = ServerRequest::fromGlobals()->withUri(new Uri($newUri));
+
+$request = $request
+    ->withoutHeader('Accept-Encoding')
+    ->withHeader('Accept-Encoding', 'gzip, deflate');
+
+if ($boundaryName = getBoundary($request)) {
+    $request = setMultipart($boundaryName, $request);
+}
+
+$response = send($request);
+
+$response = $response
+    ->withoutHeader('Transfer-Encoding');
+
+(new SapiStreamEmitter())->emit($response);
